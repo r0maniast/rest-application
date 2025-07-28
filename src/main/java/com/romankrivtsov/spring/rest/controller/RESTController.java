b229b79a93cc438d6a8ed94a6e7f3f4a6fe49ae4
r0maniast <romankrivtsov7@gmail.com>
2025-07-28 13:23:29 +0300

    create RESTController.java

---
+++ src/main/java/com/romankrivtsov/spring/rest/controller/RESTController.java
@@ -0,0 +1,45 @@
+package com.romankrivtsov.spring.rest.controller;
+
+import com.romankrivtsov.spring.rest.entity.Employee;
+import com.romankrivtsov.spring.rest.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class RESTController {
+
+    private final EmployeeService employeeService;
+
+    public RESTController(@Autowired EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> showAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable("id") int id) {
+        return employeeService.getEmployeeById(id);
+    }
+
+    @PostMapping("/employees")
+    public Employee addNewEmployee(@RequestBody Employee employee) {
+        return employeeService.saveEmployee(employee);
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee){
+        return employeeService.saveEmployee(employee);
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public String deleteEmployee(@PathVariable("id") int id){
+        employeeService.deleteEmployee(id);
+        return "Employee with id = " + id + " was deleted";
+    }
+}
